69d4145c43cc3addd85cb86e6937333a7db28016
Rewa Patil <>
2021-05-12 14:23:19 +0000

    var

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ let counter = 0
 let number1 = 0
 let number2 = 0
 function multiply () {
-  const number1 = parseInt(document.getElementById('num1').value)
-  const number2 = parseInt(document.getElementById('num2').value)
+   number1 = document.getElementById('num1').value
+   number1 = parseInt(number1)
+   number2 = document.getElementById('num2').value
+   number2 = parseInt(number2)
 
   for (counter = 0; counter < numb1; counter++) {
     answer = answer + num2
