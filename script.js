8396ec26a690d1a39aa326075d746baa4c7a6e9c
Rewa Patil <>
2021-05-12 14:56:44 +0000

    added extra

--- script.js
+++ script.js
@@ -3,13 +3,31 @@ let answer = 0
 let counter = 0
 let number1 = 0
 let number2 = 0
+let negative = false
 function multiply () {
   number1 = document.getElementById('num1').value
   number1 = parseInt(number1)
   number2 = document.getElementById('num2').value
   number2 = parseInt(number2)
+  answer = 0
+  negative = false
+
+  if (number1 < 0) {
+    negative = !negative
+    number1 = Math.abs(number1)
+  }
+
+  if (number2 < 0) {
+    negative = !negative
+     number2 = Math.abs(number2)
 
   for (counter = 0; counter < number1; counter++) {
     answer = answer + number2
-  } document.getElementById('answer').innerHTML = answer
+  }
+  
+  if (negative) {
+    answer = 0 - answer
+  }
+  document.getElementById('answer').innerHTML = answer
+}
 }
