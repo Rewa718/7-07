b9353092011f1595b9a38e747c6a15cc74315cb2
Rewa Patil <>
2021-05-12 14:28:57 +0000

    indent & var

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ let counter = 0
 let number1 = 0
 let number2 = 0
 function multiply () {
-   number1 = document.getElementById('num1').value
-   number1 = parseInt(number1)
-   number2 = document.getElementById('num2').value
-   number2 = parseInt(number2)
+  number1 = document.getElementById('num1').value
+  number1 = parseInt(number1)
+  number2 = document.getElementById('num2').value
+  number2 = parseInt(number2)
 
-  for (counter = 0; counter < numb1; counter++) {
-    answer = answer + num2
+  for (counter = 0; counter < number1; counter++) {
+    answer = answer + number2
   } document.getElementById('answer').innerHTML = answer
 }
