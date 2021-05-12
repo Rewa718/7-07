33cb734f031088f5934a197ae2c704ab01e3346b
Rewa Patil <>
2021-05-12 14:06:56 +0000

    commmit

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 document.getElementById('calculate').addEventListener('click', multiply)
 let answer = 0
+let counter = 0
 function multiply () {
-  const num1 = parseInt(document.getElementById('num1').value)
+  const num1 = parseInt(document.getElementById('number1').value)
   const num2 = parseInt(document.getElementById('num2').value)
 
-  for (counter = 0; counter < num1; counter++) {
+  for (counter = 0; counter < number1; counter++) {
     answer = answer + num2
-  }
-  document.getElementById('answer').innerHTML = answer
-
-
+  } document.getElementById('answer').innerHTML = answer
   }
