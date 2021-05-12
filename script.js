7c0243d9186b016e990bfc04cb417a16f8d03beb
Rewa Patil <>
2021-05-12 14:13:51 +0000

    change var

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 document.getElementById('calculate').addEventListener('click', multiply)
 let answer = 0
 let counter = 0
+let number1 = 0
+let number2 = 0
 function multiply () {
-   num1 = parseInt(document.getElementById('numb1').value)
-  const num2 = parseInt(document.getElementById('num2').value)
+   numner1 = parseInt(document.getElementById('num1').value)
+  number2 = parseInt(document.getElementById('num2').value)
 
   for (counter = 0; counter < numb1; counter++) {
     answer = answer + num2
